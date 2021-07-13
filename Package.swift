fceaeb3e081aab4413011b30fb6999a35257b092
Simon <simon2204s@gmail.com>
2021-07-14 00:34:58 +0200

    downgraded package version

--- Package.swift
+++ Package.swift
@@ -1,23 +1,24 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MoodleAPI",
-    platforms: [.macOS(.v10_12)],
+    platforms: [.macOS(.v10_11)],
     products: [
         .library(name: "MoodleAPI", targets: ["MoodleAPI"])
     ],
     dependencies: [
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.2"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
-        .package(url: "https://github.com/simon2204/FormData.git", branch: "main")
+        .package(url: "https://github.com/simon2204/FormData.git", .branch("main"))
     ],
     targets: [
         .target(
             name: "MoodleAPI",
-            dependencies: ["SwiftSoup", "ZIPFoundation", "FormData"]),
+            dependencies: ["SwiftSoup", "ZIPFoundation", "FormData"],
+            exclude: ["MoodleAPI.docc"]),
         .testTarget(
             name: "MoodleAPITests",
             dependencies: ["MoodleAPI"]),
